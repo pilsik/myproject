39ee272240ac9bdf89a29180f62cd10530fc9f31
pilsik <pilsik1991@gmail.com>
2017-03-13 20:12:55 +0300

    add web

---
+++ web/src/main/java/by/IvkoS/web/controller/MainController.java
@@ -0,0 +1,20 @@
+package by.IvkoS.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+    protected final static Logger logger = LoggerFactory.getLogger(MainController.class);
+
+    @RequestMapping("/")
+    public ModelAndView index() {
+        logger.info("Returning index view");
+        return new ModelAndView("index");
+    }
+
+}
